0c0b3a8e983d164bbc3ceb252a787551aa15855c
Jonathan K <jkamagi41@gmail.com>
2024-04-16 18:19:21 +0800

    Layouting statistik wilayah dan paroki fragment

--- app/src/main/java/com/example/healthcare/ui/dashboard/StatistikWilayahFragment.kt
+++ app/src/main/java/com/example/healthcare/ui/dashboard/StatistikWilayahFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.healthcare.databinding.FragmentDashboardBinding
+import com.example.healthcare.databinding.FragmentStatistikWilayahBinding
 
-class DashboardFragment : Fragment() {
+class StatistikWilayahFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentStatistikWilayahBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +24,10 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentStatistikWilayahBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
